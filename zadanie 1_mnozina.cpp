b4e136c4c32681f522b021ad8e670a60d11a1eee
ArtLeg <art.legeida@gmail.com>
2023-06-16 15:25:50 +0200

    7 (zmenil optimizer)

--- zadanie 1_mnozina.cpp
+++ zadanie 1_mnozina.cpp
@@ -54,7 +54,7 @@ Mnozina* copy(Mnozina* mn)
 Mnozina* remove(Mnozina *mn, int index)
 {
 	if (mn->size == 0) {
-		printf("No elements left\n");
+		//printf("No elements left\n");
 		return(mn);
 	}
 	for (int i = index; i < mn->size - 1; i++)
@@ -74,130 +74,6 @@ void print(Mnozina* mn)
 	}
 	printf("\n");
 }
-
-void merge(int *arr, int l, int mid, int r) {
-	int i, j, k;
-	const int n1 = mid - l + 1;  
-	const int n2 = r - mid;       
-
-	int* L = (int*)malloc(n1 * sizeof(int));
-	if (L == NULL) {
-		//printf("Memory allocation problem\n");
-		merge(arr, l, mid, r);
-	}
-	int* R = (int*)malloc(n2 * sizeof(int));
-	if (R == NULL) {
-		//printf("Memory allocation problem\n");
-		free(L);
-		merge(arr, l, mid, r);
-	}
-
-	for (i = 0; i < n1; i++)
-		L[i] = arr[l + i];
-	for (j = 0; j < n2; j++)
-		R[j] = arr[mid + 1 + j];
-
-	i = 0;   
-	j = 0;   
-	k = l;   
-
-	while (i < n1 && j < n2) {
-		if (L[i] <= R[j]) {
-			arr[k] = L[i];
-			i++;
-		}
-		else {
-			arr[k] = R[j];
-			j++;
-		}
-		k++;
-	}
-
-	while (i < n1) {
-		arr[k] = L[i];
-		i++;
-		k++;
-	}
-
-	while (j < n2) {
-		arr[k] = R[j];
-		j++;
-		k++;
-	}
-}
-
-void mergeSort(int *arr, int l, int r) {
-	if (l < r) {
-		int mid = l + (r - l) / 2;
-		mergeSort(arr, l, mid);
-		mergeSort(arr, mid + 1, r);
-		merge(arr, l, mid, r);
-	}
-}
-
-Mnozina* optimize(Mnozina* mn)
-{
-	mergeSort(mn->arr, 0, mn->size - 1);
-	int count = 0;
-	int ttemp = 0;
-	Mnozina* nTemp = konstructor();
-	if (nTemp == NULL) {
-		//printf("Memory allocation problem\n");
-		optimize(mn);
-	}
-	for (int i = 0; i < mn->size; i++)
-	{
-		if (i == 0)
-		{
-			free(nTemp->arr);
-			nTemp->arr = (int*)malloc(1 * sizeof(int));
-			if (nTemp->arr == NULL) {
-				//printf("Memory allocation problem\n");
-				destrucktor(nTemp);
-				optimize(mn);
-			}
-			nTemp->arr[nTemp->size] = mn->arr[i];
-			nTemp->size++;
-			ttemp = mn->arr[i];
-		}
-		else
-		{
-			if (ttemp != mn->arr[i])
-			{
-				nTemp->size++;
-				Mnozina* temp = copy(nTemp);
-				free(nTemp->arr);
-				nTemp->arr = (int*)malloc((nTemp->size) * sizeof(int));
-				if (nTemp->arr == NULL) {
-					//printf("Memory allocation problem\n");
-					destrucktor(nTemp);
-					optimize(mn);
-				}
-				for (int j = 0; j < temp->size; j++)
-				{
-					nTemp->arr[j] = temp->arr[j];
-				}
-				nTemp->arr[nTemp->size - 1] = mn->arr[i];
-				ttemp = mn->arr[i];
-			}
-		}
-	}
-	destrucktor(mn);
-	mn = nTemp;
-	return(mn);
-}
-
-Mnozina* pop_back(Mnozina* mn)
-{
-	if (mn->size == 0) {
-		printf("No elements left\n");
-		return(mn);
-	}
-	mn->size--;
-	return(mn);
-}
-
-
 
 void push_back(Mnozina* mn, int a)
 {
@@ -261,6 +137,88 @@ void push_back(Mnozina* mn)
 	mn->arr[mn->size - 1] = a;
 	destrucktor(temp);
 }
+
+void merge(int *arr, int l, int mid, int r) {
+	int i, j, k;
+	const int n1 = mid - l + 1;  
+	const int n2 = r - mid;       
+
+	int* L = (int*)malloc(n1 * sizeof(int));
+	int* R = (int*)malloc(n2 * sizeof(int));
+
+	for (i = 0; i < n1; i++)
+		L[i] = arr[l + i];
+	for (j = 0; j < n2; j++)
+		R[j] = arr[mid + 1 + j];
+
+	i = 0;   
+	j = 0;   
+	k = l;   
+
+	while (i < n1 && j < n2) {
+		if (L[i] <= R[j]) {
+			arr[k] = L[i];
+			i++;
+		}
+		else {
+			arr[k] = R[j];
+			j++;
+		}
+		k++;
+	}
+
+	while (i < n1) {
+		arr[k] = L[i];
+		i++;
+		k++;
+	}
+
+	while (j < n2) {
+		arr[k] = R[j];
+		j++;
+		k++;
+	}
+	free(L);
+	free(R);
+}
+
+void mergeSort(int *arr, int l, int r) {
+	if (l < r) {
+		int mid = l + (r - l) / 2;
+		mergeSort(arr, l, mid);
+		mergeSort(arr, mid + 1, r);
+		merge(arr, l, mid, r);
+	}
+}
+
+Mnozina* optimize(Mnozina* mn)
+{
+	mergeSort(mn->arr, 0, mn->size - 1);
+	Mnozina* sorted = konstructor();
+	int last = -1;
+	for (int i = 0; i < mn->size; i++) {
+		if (last != mn->arr[i]) {
+			push_back(sorted, mn->arr[i]);
+			last = mn->arr[i];
+		}
+	}
+	destrucktor(mn);
+	return(sorted);
+}
+
+Mnozina* pop_back(Mnozina* mn)
+{
+	if (mn->size == 0) {
+		//printf("No elements left\n");
+		return(mn);
+	}
+	mn->size--;
+	return(mn);
+}
+
+
+
+
 
 Mnozina* ziednotenie(Mnozina* a, Mnozina* a2/*int* a, int* a2, int* arr, int* p*/)
 {
